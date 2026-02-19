a3da169e3b264969c1a7395f28b68954cfbd0fbd
jungju <leejungju.go@gmail.com>
2026-02-20 01:53:35 +0900

    build(deploy): configure gh-pages base and prerender

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.env.NODE_ENV === 'development';
+const githubPages = process.env.GH_PAGES === '1';
 
 export default {
-  kit: {
-    adapter: adapter({
-		fallback: '404.html'
-	}),
-	paths: {
-		base: ''
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		prerender: {
+			entries: ['/']
+		},
+		paths: {
+			base: githubPages ? '/jjgo' : ''
+		}
 	}
-  }
 };
