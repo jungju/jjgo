a25ba2298c1292f674b709335e287812a17de425
jungju <leejungju.go@gmail.com>
2026-02-20 02:00:41 +0900

    Updates

--- _app/immutable/chunks/BI0JxQkZ.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1chxpn8?.base??"/jjgo",a=globalThis.__sveltekit_1chxpn8?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/DdDH9v0M.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_utnstt?.base??"/jjgo",t=globalThis.__sveltekit_utnstt?.assets??s??"";export{t as a,s as b};
