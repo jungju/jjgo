33bf6dc32db11e99c8140aea30f22e3b9ae076b4
JJGo <1478395316985397338+jjgo@users.noreply.github.com>
2026-03-04 21:39:23 +0900

    Updates

--- _app/immutable/chunks/DdDH9v0M.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_utnstt?.base??"/jjgo",t=globalThis.__sveltekit_utnstt?.assets??s??"";export{t as a,s as b};
---
+++ _app/immutable/chunks/DhGbEwru.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1n4cni6?.base??"/jjgo",a=globalThis.__sveltekit_1n4cni6?.assets??s??"";export{a,s as b};
